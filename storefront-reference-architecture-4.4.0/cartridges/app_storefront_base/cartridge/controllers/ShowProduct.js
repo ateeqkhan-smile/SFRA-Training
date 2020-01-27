80815cf5fbde44ca255e795a45bbf1b38b76a8b9
atulbedse <atul.bedse@happiestminds.com>
2020-01-27 14:21:23 +0530

    task1. finding product using pid completed where two isml files(trainingTemplate) and controller(showproduct) is made

--- storefront-reference-architecture-4.4.0/cartridges/app_storefront_base/cartridge/controllers/ShowProduct.js
+++ storefront-reference-architecture-4.4.0/cartridges/app_storefront_base/cartridge/controllers/ShowProduct.js
@@ -10,8 +10,7 @@ server.get('Show', cache.applyDefaultCache, function (req, res, next) {
 	var priceModel = product.getPriceModel();
 	var listPrice1	= parseInt(priceModel.getPriceBookPrice(priceModel.getPriceInfo().getPriceBook().getParentPriceBook().getID()).toString().replace("USD", ""));
 	var salesPrice = parseInt(priceModel.getPrice().toString().replace("USD", "")) ;
-	var discount = listPrice1 - salesPrice;
-	var discountPercentage = Math.round((discount / listPrice1) * 100);
+	var discountPercentage = Math.round(((listPrice1 - salesPrice) / listPrice1) * 100);
 
 	if(product === null){
 		   res.render('/trainingTemplate/productnotfound', {message: "product not found "+req.querystring.pid}); 
